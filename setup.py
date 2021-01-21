1a63bba6a474697ca0bd8b71f514102050b8d4d0
Mia Bonardi <65858862+miabonardi@users.noreply.github.com>
2021-01-21 16:56:22 -0500

    Second commit

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.ECHR',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.ECHR\n\nA docassemble extension.\n\n## Author\n\nMia Bonardi, mbonardi@su.suffolk.edu\n\n',
+      long_description='# docassemble.ECHR\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nMia Bonardi, mbonardi@su.suffolk.edu\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Mia Bonardi',
       author_email='mbonardi@su.suffolk.edu',
